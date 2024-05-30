95c913527153cb010eb86fccd61e9da301f336c1
jin <jin200126@gmail.com>
2024-05-31 00:17:26 +0900

    register

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const indexRouter = require('./routes/index')
 const app = express()
 
 require('dotenv').config()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))     // json말고 다른 형태의 문서를 취급하기 위해 추가
 app.use(bodyParser.json())      // 이게 있어야 req.body가 객체로 인식된다.
+
+app.use('/api', indexRouter)
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS
 mongoose.connect(mongoURI, { useNewUrlParser: true })
---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
+const userController = {}
+
+userController.createUser = async (req, res) => {
+    try {
+        let { email, name, password, level } = req.body
+        const existUser = await User.findOne({ email })          // 전달 받은 이메일로 기존 가입이 있는지 확인하기
+        if (existUser) {
+            throw new Error("user already exist")
+        }
+
+        const salt = await bcrypt.genSaltSync(10)
+        password = await bcrypt.hash(password, salt)            // 암호화된 password로 재정의
+        const newUser = new User({ email, password, name, level: level ? level : 'customer' })
+        await newUser.save()
+        return res.status(200).json("createUser success")
+
+    } catch (error) {
+        res.status(400).json({ status: "createUser fail", error: error.message })
+    }
+}
+
+module.exports = userController
+
