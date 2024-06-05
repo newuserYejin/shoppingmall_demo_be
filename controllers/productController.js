4a16887faeefc8c201d3f8b2474830328c065177
jin <jin200126@gmail.com>
2024-06-06 01:53:20 +0900

    adminpage product add

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,4 +14,14 @@ productController.createProduct = async (req, res) => {
     }
 }
 
+productController.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json({ status: "get product success", data: products })
+
+    } catch (error) {
+        res.status(400).json({ status: "item save fail", error: e.message })
+    }
+}
+
 module.exports = productController
--- routes/product_api.js
+++ routes/product_api.js
@@ -6,4 +6,6 @@ const productController = require('../controllers/productController')
 router.post('/', authController.authenticate, authController.checkAdminPermission, productController.createProduct)
 // token 으로부터 userId를 받아올 수 있는 미들웨어, admin권한이 있는지 확인하는 미들웨어 설정
 
+router.get('/', productController.getProducts)
+
 module.exports = router
